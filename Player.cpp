9d41f2756b9c4c7e71e5d2c61297e4d69d27f95c
BlagojeM <blagoje.mirkovic@gmail.com>
2019-01-18 00:49:34 +0100

    .gitignore ignored a important file

--- Player.cpp
+++ Player.cpp
@@ -5,12 +5,21 @@ Player::Player() : Transformable(this) {
 }
 
 void Player::Draw(float alpha){
-		glColor3f(0.5, 0, 0);
 		glTranslatef(0, 0, 0.13);
 		if(alpha)
 			glTranslatef(0, 0, -1);
+		glColor3f(0.5, 0, 0);
 		glutSolidCube(0.1);
 		glScalef(1, 3, 1);
 		glTranslatef(0, -0.08, 0);
 		glutSolidCube(0.1);
+		glTranslatef(0, 0.14, 0);
+		glScalef(0.03, 0.017, 0.03);
+		glColor3f(0, 1, 0);
+		glRotatef(90, 0, 1, 0);
+		glutSolidTetrahedron();
+		glTranslatef(0, -0.9, 0);
+		glRotatef(180, 1, 0, 0);
+		glutSolidTetrahedron();
+
 }
